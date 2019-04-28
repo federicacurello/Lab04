a2408f88900364346376075c5bba84b0017be787
federica.curello <feffy22@live.it>
2019-04-28 19:27:13 +0200

    Fatta interfaccia grafica

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/controller/SegreteriaStudentiController.java
@@ -1,5 +1,93 @@
+
+/**
+ * Sample Skeleton for 'SegreteriaStudenti.fxml' Controller Class
+ */
+
 package it.polito.tdp.lab04.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class SegreteriaStudentiController {
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="menuCorso"
+    private ComboBox<String> menuCorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaIscritti"
+    private Button btnCercaIscritti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMatricola"
+    private TextField txtMatricola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaCorsi"
+    private Button btnCercaCorsi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnIscrivi"
+    private Button btnIscrivi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtArea"
+    private TextArea txtArea; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnReset"
+    private Button btnReset; // Value injected by FXMLLoader
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscritti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaNome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSceltaCorso(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert menuCorso != null : "fx:id=\"menuCorso\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert txtArea != null : "fx:id=\"txtArea\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'SegreteriaStudenti.fxml'.";
+
+    }
 }
+
